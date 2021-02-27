f98121b2b38dc4f0a5a58ffab0f190c3da4ef42e
Gavin Stark <gavin@gstark.com>
2021-02-26 20:32:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Suncoast Developers Guild CORS Proxy
+
+A simple nodejs based proxy for students of Suncoast Developers Guild
+
+Based on: [cors-anywhere](https://github.com/Rob--W/cors-anywhere)
---
+++ server.js
@@ -0,0 +1,53 @@
+// Listen on a specific host via the HOST environment variable
+const host = process.env.HOST || '0.0.0.0'
+
+// Listen on a specific port via the PORT environment variable
+const port = process.env.PORT || 8080
+
+// Grab the blacklist from the command-line so that we can update the blacklist without deploying
+// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
+// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
+// use originWhitelist instead.
+const originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST)
+const originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST)
+function parseEnvList(env) {
+  if (!env) {
+    return []
+  }
+  return env.split(',')
+}
+
+// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
+const checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT)
+
+const corsProxy = require('./lib/cors-anywhere')
+
+corsProxy
+  .createServer({
+    originBlacklist: originBlacklist,
+    originWhitelist: originWhitelist,
+    requireHeader: ['origin', 'x-requested-with'],
+    checkRateLimit: checkRateLimit,
+    removeHeaders: [
+      'cookie',
+      'cookie2',
+      // Strip Heroku-specific headers
+      'x-request-start',
+      'x-request-id',
+      'via',
+      'connect-time',
+      'total-route-time',
+      // Other Heroku added debug headers
+      // 'x-forwarded-for',
+      // 'x-forwarded-proto',
+      // 'x-forwarded-port',
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+      xfwd: false,
+    },
+  })
+  .listen(port, host, function () {
+    console.log('Running CORS Anywhere on ' + host + ':' + port)
+  })
